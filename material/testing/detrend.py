c8ea1e63fa1258aa548bce1e6d25e7094dbe49f5
unknown <a.loew@IGGF-LPEDGE540.iggf.geo.uni-muenchen.de>
2017-03-30 10:49:40 +0200

    modified detrend

--- material/testing/detrend.py
+++ material/testing/detrend.py
@@ -4,12 +4,16 @@ detrend a timeseries
 
 import numpy as np
 
-from scipy import stats
-
-def xx_detrend(x,y):
-    N = len(x)
-    return np.random.random(N), 0., 0.
+#from scipy import stats
 
 def detrend(x,y):
+    N = len(x)
+   
+
+
+    return np.zeros(N), 5, 2.3
+    #return np.random.random(N), 0., 0.
+
+def xx_detrend(x,y):
     slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
     return y - (slope*x + intercept), slope, intercept
